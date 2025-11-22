2ec4704c326809a075952803027cb0eb4606ebbd
alapnian <dyowiranatamulya@gmail.com>
2025-11-23 02:20:35 +0700

    Upload Speech Catalog
    Data:
    1	Language Families	118 records
    2	Language Scopes	3 records
    3	Language Scripts	29 records
    4	Language Types	6 records
    5	Languages	183 records
    6	Locales	413 records
    
    Formats:
    - json
    - php
    - xml
    - xslx

---
+++ assets/data/php/lang_scopes.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace DyoWiranataMulya\DataCatalogs\Data;
+
+return [['id'=>1,'name'=>'individual language','description'=>'Normal, single language.',],['id'=>2,'name'=>'macrolanguage','description'=>'One-to-many grouping of languages, because older ISO 639s included them.',],['id'=>3,'name'=>'special code','description'=>'Non-language codes.',],];
